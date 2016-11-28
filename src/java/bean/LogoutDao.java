e044d94f26a709efc89f438b298e5f968b5cd320
Lock <Lock@192.168.1.3>
2016-11-28 05:39:33 +0530

    chat back end adding from database when user logs in and adds chats back to database when logout

---
+++ src/java/bean/LogoutDao.java
@@ -0,0 +1,26 @@
+package bean;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lock
+ */
+public class LogoutDao {
+
+    public static void logout(int userId) {
+        Connection con = ConnectionProvider.getCon();
+        List<Message> rList=Chat.recievedMessages.remove(userId);
+        List<Message> sList=Chat.sentMessages.remove(userId);
+        
+        for(Message m:rList){
+            
+        }
+        
+        for(Message m:rList){
+            
+        }
+    }
+}
--- web/logout.jsp
+++ web/logout.jsp
@@ -1,4 +1,6 @@
+<%@page import="bean.LogoutDao"%>  
 <% 
+    LogoutDao.logout((int)session.getAttribute("user_id"));
    session.setAttribute("session", "FALSE");
    session.invalidate();
    
